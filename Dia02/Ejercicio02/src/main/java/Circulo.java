44ef7a5c80b98b80854a27c63c271bd3c001ac66
JeanMa <cjeanma009@gmail.com>
2020-01-18 21:46:12 -0500

    Ejercicio 02 java

---
+++ Dia02/Ejercicio02/src/main/java/Circulo.java
@@ -0,0 +1,21 @@
+
+public class Circulo {
+    private double radio; 
+    private static final double PI = 3.1416;
+    
+    public Circulo(double radio){
+        this.radio = radio;
+    }
+    public double calcularArea(){
+        return Math.pow(PI*this.radio,2);
+    }
+
+    public double getRadio() {
+        return radio;
+    }
+
+    public void setRadio(double radio) {
+        this.radio = radio;
+    }
+    
+}
