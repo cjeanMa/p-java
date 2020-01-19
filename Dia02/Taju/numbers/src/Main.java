5424e3d6ef39dfffe0ee0f2c44715ebd59140e35
cjalirip <cjalirip@everis.com>
2020-01-19 14:43:05 -0500

    Day 2, compare numbs

--- Dia02/Taju/numbers/src/Main.java
+++ Dia02/Taju/numbers/src/Main.java
@@ -7,12 +7,29 @@ public class Main {
         var num1 = scanner.nextInt();
         System.out.println("Ingrese otro numero");
         var num2 = scanner.nextInt();
-        NumbersApp compare = new NumbersApp(num1, num2);
+        System.out.println("Ingrese otro numero");
+        var num3 = scanner.nextInt();
+        NumbersApp compare = new NumbersApp(num1, num2, num3);
 
+        // Comparación de dos números
         if (compare.isEqual()) {
             System.out.println("Los números " + num1 + " y " + num2 + " son iguales");
         } else {
             System.out.println("Los números " + num1 + " y " + num2 + " no son iguales");
+        }
+
+        // Validar si un número es positivo o no
+        if (compare.isPositive()) {
+            System.out.println("El número " + num3 +  " es positivo");
+        } else {
+            System.out.println("El número " + num3 +  " es negativo");
+        }
+
+        // Verificar si dos números son múltiplos
+        if (compare.isMultiple()) {
+            System.out.println("Los números " + num1 + " y " + num2 + " son múltiplos");
+        } else {
+            System.out.println("Los números " + num1 + " y " + num2 + " no son múltiplos");
         }
     }
 }
--- Dia02/Taju/numbers/src/NumbersApp.java
+++ Dia02/Taju/numbers/src/NumbersApp.java
@@ -3,12 +3,21 @@ public class NumbersApp {
     private int num2;
     private int num3;
 
-    public NumbersApp (int num1, int num2) {
+    public NumbersApp (int num1, int num2, int num3) {
         this.num1 = num1;
         this.num2 = num2;
+        this.num3 = num3;
     }
 
     public boolean isEqual() {
         return this.num1 == this.num2;
+    }
+
+    public boolean isPositive() {
+        return this.num3 > 0;
+    }
+
+    public boolean isMultiple() {
+        return (this.num1 % this.num2) == 0;
     }
 }
