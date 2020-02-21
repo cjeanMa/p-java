5071f293e4f47c7d6e6c7627795492f3c647d935
cjalirip <cjalirip@everis.com>
2020-02-21 15:38:21 -0500

    Change buttons for panels, but It shows nothing, even

--- Dia04/Taju/aprendiendoSwing/README.md
+++ Dia04/Taju/aprendiendoSwing/README.md
@@ -4,3 +4,6 @@
 
 * Para el día 4 logré imprimir un array de botones en un Frame
 ![image](https://user-images.githubusercontent.com/32286691/73138205-1698b000-402e-11ea-8603-825dc41ea6bf.png)
+
+
+* Dia enésimo...
--- Dia04/Taju/aprendiendoSwing/src/swing/Main.java
+++ Dia04/Taju/aprendiendoSwing/src/swing/Main.java
@@ -1,27 +1,48 @@
 package swing;
 import javax.swing.*;
 import java.awt.*;
+import java.util.ArrayList;
 
 public class Main {
 
     public static void main(String[] args) {
-        ListButton buttons = new ListButton();
-
-        buttons.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        // miMarco marco1 = new miMarco();
+        Numbers numb = new Numbers();
+        // marco1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 }
 
-class ListButton extends JFrame {
-    JButton[] arrayBtn = new JButton[200];
-    public ListButton() {
-        setLayout(new GridLayout(0, 10));
-        setVisible(true);
-        setSize(480, 320);
-        setLocation(500, 300);
-        setTitle("Hola Mundo");
-        for ( int i = 0; i < arrayBtn.length; i++) {
-            arrayBtn[i] = new JButton("" + (i + 1));
-            add(arrayBtn[i]);
+class Numbers extends JFrame {
+    JPanel container;
+    ArrayList<JPanel> panels;
+
+    public Numbers() {
+        arrayList();
+    }
+
+    public void arrayList() {
+
+        container = new JPanel();
+        container.setVisible(true);
+        container.setLayout(new GridLayout(0, 6));
+        container.setPreferredSize(new Dimension(300, 500));
+
+
+
+        panels = new ArrayList();
+        for (int i = 0; i < 60; i ++) {
+            System.out.println(i);
+            JPanel panel = new JPanel(new FlowLayout(FlowLayout.CENTER));
+            JLabel number = new JLabel("" + (i + 1));
+            panel.setBorder(BorderFactory.createEtchedBorder());
+            panel.add(number);
+            panels.add(panel);
         }
+
+        panels.forEach(cons -> {
+            System.out.println(cons);
+            container.add(cons);
+        });
     }
 }
+
